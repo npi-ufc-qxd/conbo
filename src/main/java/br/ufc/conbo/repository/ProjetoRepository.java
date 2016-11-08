b6b7b8f76ee181a8c314e38d97221544589c295f
andreza.sb <andrezadesouzabrito@gmail.com>
2016-11-08 15:11:51 -0300

    Crud Projeto

--- src/main/java/br/ufc/conbo/repository/ProjetoRepository.java
+++ src/main/java/br/ufc/conbo/repository/ProjetoRepository.java
@@ -1,12 +1,19 @@
 package br.ufc.conbo.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import br.ufc.conbo.model.Projeto;
 
 @Repository
 @Transactional
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+	
+	@Query("from Projeto p where (lower(p.nome) like :busca)")
+	List<Projeto> buscarPorNome(@Param("busca") String busca);
 
 }

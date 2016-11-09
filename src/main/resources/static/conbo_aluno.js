d4a94452569c4f21045151ab11642391444b12ab
05961436306 <felipe76857685@gmail.com>
2016-11-09 17:30:03 -0300

    Commit de segurança diário

--- src/main/resources/static/conbo_aluno.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){ 
-	  $('#CPF').mask('999.999.999-99', {reverse: true});
-});
-
-
---
+++ src/main/resources/static/js/aluno/cadastrar.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ 
+	  $('#CPF').mask('000.000.000-00', {reverse: false});
+});
+
+
